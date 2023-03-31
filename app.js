d090ab68ef84e01edbd440852be3c471d156331a
khushal <khushaljain356@gamil.com>
2023-03-31 15:41:48 +0530

    5.13

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const bodyParser=require('body-parser');
 const app=express();
 app.use(bodyParser.urlencoded({extended:false}));
 
-app.use(adminRoutes);
+app.use('/admin',adminRoutes);
 app.use(shoproutes);
 
 
--- routes/admin.js
+++ routes/admin.js
@@ -2,11 +2,12 @@ const express=require('express');
 
 const router=express.Router();
 
-
+//filtering when two routes have same address if they have different method 
+//if all routes here are following some common address then we can use filtering here like /admin/add-product let suppose all routes here are starting with /admin then we can add admin at app.js file where we are calling this
 router.get('/add-product',(req,res,next)=>{
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">submit</input></form>')
+    res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">submit</input></form>')
 })
-router.post('/product',(req,res,next)=>{
+router.post('/add-product',(req,res,next)=>{
     console.log(req.body);
      res.redirect('/');
 
